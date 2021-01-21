313822079d345d0c50b6831212d549008b193f71
JakeT23 <69044307+JakeT23cool@users.noreply.github.com>
2021-01-21 10:07:56 +0000

    Update submit.php

--- src/messageHandler/submit.php
+++ src/messageHandler/submit.php
@@ -1,8 +1,8 @@
 <?php
-	$id = 00000;
+	$id = 00000; // id system
 	
-	$data_file = fopen("message.txt", "a+");
-	$name = $_POST["name"];
+	$data_file = fopen("message.txt", "a+"); /
+	$name = $_POST["name"]; 
 	$message = $_POST["message"];
 	$text_to_write = $id . " "  . $name . ": " . $message . "\n";
 	fwrite($data_file, $text_to_write);
