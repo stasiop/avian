7d5526b6e98a0192d78dc6cea6c34551b8ddb54f
JakeT23 <69044307+JakeT23cool@users.noreply.github.com>
2021-01-26 09:18:18 +0000

    Update submit.php

--- src/messageHandler/submit.php
+++ src/messageHandler/submit.php
@@ -10,7 +10,6 @@
 	$name = $_POST["name"];
 	$message = $_POST["message"];
 	$text_to_write = $ID4 . " " . "Anon "  . $name . ": " . $message . ";";
-	fwrite('id.txt', $ID);
 	fwrite($data_file, $text_to_write);
 	fclose($data_file);
 	header('Location: /anonchat.html');
