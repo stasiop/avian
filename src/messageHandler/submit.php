1b0f8afed9e147fb06b2346f01e2752c25e867d4
JakeT23 <69044307+JakeT23cool@users.noreply.github.com>
2021-01-28 10:50:34 +0000

    Update submit.php

--- src/messageHandler/submit.php
+++ src/messageHandler/submit.php
@@ -2,14 +2,15 @@
 
 	function saveFile(){
 	$IDstr = file_get_contents("id.txt");
-	$ID = (int) $IDstr;
+	$IDstr = (int) $ID;
 	$ID++;
 	$ID4 = sprintf("%04s", $ID);
 	$data_file = fopen("message.txt", "a+");
 	$IDsave = fopen("id.txt", "w+");
 	$name = $_POST["name"];
 	$message = $_POST["message"];
-	$text_to_write = $ID4 . " " . "Anon "  . $name . ": " . $message . "\n";
+	$text_to_write = /* $ID4. " " . */ "Anon "  . $name . ": " . $message . ";";
+	fwrite('id.txt', $ID);
 	fwrite($data_file, $text_to_write);
 	fclose($data_file);
 	header('Location: /anonchat.html');
