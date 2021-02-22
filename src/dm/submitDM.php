037ee09ff51cbccb2b15f7ba9eef49a850199818
stasiop <51001942+stasiop@users.noreply.github.com>
2021-02-22 18:38:18 +0000

    Update submitDM.php

--- src/dm/submitDM.php
+++ src/dm/submitDM.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
-$name = $_COOKIE['user']
-$data_file = fopen("/src/dm/" . $_SESSION['DMnum'] . ".txt", "a+");
+$name = $_COOKIE['user'];
+$data_file = fopen($_SESSION['DMnum'] . ".txt", "a+");
 $message = $_POST["message"];
 $text_to_write = $name . ": " . $message . "\n";
 fwrite($data_file, $text_to_write);
 fclose($data_file);
-header('Location: /src/dm/dm_finder.php');
+header('Location: dm_finder.php');
 ?>
