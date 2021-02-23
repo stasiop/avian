27fa74f08b5eb6a0dfa8cb7f6ba69a5d9513c7d0
stasiop <51001942+stasiop@users.noreply.github.com>
2021-02-23 08:58:31 +0000

    Update dm_finder.php

--- src/dm/dm_finder.php
+++ src/dm/dm_finder.php
@@ -7,6 +7,7 @@ if (!isset($_SESSION['DMnum'])) {
 	$_SESSION['DMnum'] = $_COOKIE['user'];
 }
 ?>
+<link type="text/css" rel="stylesheet" href="/Stylesheet.css">
 <body onload="Autoscroll();">
 	<iframe id="GFG" src = '/src/dm/<?php echo $_SESSION['DMnum'];?>.txt' width = "50%" height = "50%"style="border: 0px">
 		Sorry your browser does not support inline frames. Use GNU IceCat.
@@ -17,7 +18,6 @@ if (!isset($_SESSION['DMnum'])) {
     </form>
 	<script>
 		function Autoscroll() {			// this is good
-		document.getElementById('GFG').contentWindow.location.reload();
 		var iframeID = document.getElementById("GFG");
 
 		var iframeCW = (iframeID.contentWindow || iframeID.contentDocument);
