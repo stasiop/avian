4acadcabf47365a065f403ae2bb96513b0ee7783
stasiop <51001942+stasiop@users.noreply.github.com>
2021-02-23 09:40:51 +0000

    Update submit.php

--- src/messageHandler/submit.php
+++ src/messageHandler/submit.php
@@ -1,7 +1,11 @@
-<?php
-		$name = "Anon";
+<?php	
 		$data_file = fopen("message.txt", "a+");
 		$message = $_POST["message"];
+		if (isset($_COOKIE['user'])) {
+			$name = "Anon (" . $_COOKIE['user'] . ")";
+		} else {
+			$name = "Anon";
+		}
 		$text_to_write = $name . ": " . $message . "\n";
 		fwrite($data_file, $text_to_write);
 		fclose($data_file);
