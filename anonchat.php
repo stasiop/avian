91fcadd68f6b7a217fef36ad7807ebe7697a744e
stasiop <51001942+stasiop@users.noreply.github.com>
2021-02-23 16:58:58 +0000

    Update anonchat.php

--- anonchat.php
+++ anonchat.php
@@ -52,6 +52,50 @@
                                         task()
                                         
                                 </script>                         
+                                      <iframe id="GFG" src = "/src/messageHandler/message.html" width = "100%" height = "100%"style="border: 0px">
+                                        Sorry your browser does not support inline frames. Use GNU IceCat.
+                                      </iframe>
+                        </div>
+                            <script>
+                                function Autoscroll() { // this is good
+                                var iframeID = document.getElementById("GFG");
+
+                                var iframeCW = (iframeID.contentWindow || iframeID.contentDocument);
+                                document.getElementById('GFG').onload = function(){ setTimeout("document.getElementById('GFG').contentWindow.scrollTo(0, 99999999)", 1) }
+                                if (iframeCW.document) iframeCW = iframeCW.document;
+                                iframeCW.body.style.color = "white";
+                                }
+                           </script>
+                           <script>
+                                Autoscroll();
+                            </script>
+                        <form method="POST" action="src/messageHandler/submit.php">
+
+                                <input name="message" placeholder="Isn't avian so pog?!" type="text">
+                                <button type="submit" value="submit">Send</button>
+                        </form>
+						<form action="/src/messageHandler/upload.php" method="post" enctype="multipart/form-data">
+							  Select image to upload:
+							  <input type="file" name="fileToUpload" id="fileToUpload">
+							  <input type="submit" value="Upload Image" name="submit">
+						</form>
+
+                      
+                </div>
+        </body>
+
+</html>
+
+
+
+                                                                console.log("sameID");
+                                                        };
+                                                        
+                                                };
+                                        }
+                                        task()
+                                        
+                                </script>                         
                                       <iframe id="GFG" src = "/src/messageHandler/message.txt" width = "100%" height = "100%"style="border: 0px">
                                         Sorry your browser does not support inline frames. Use GNU IceCat.
                                       </iframe>
