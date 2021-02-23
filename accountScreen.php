f0a15142b46b6795f62db000786002df5cf50a68
stasiop <51001942+stasiop@users.noreply.github.com>
2021-02-23 09:28:20 +0000

    Update accountScreen.php

--- accountScreen.php
+++ accountScreen.php
@@ -5,10 +5,11 @@ if(!isset($_COOKIE["user"])) {
 	setcookie($cookie_name, $cookie_value, time() + (86400 * 365), "/"); // 86400 = 1 day
 }
 ?>
-<table width="10%" border="1" class="table">
+<table border="1" class="table">
     <tr>
         <td><a href="/anonchat.php">anonchat</a></td>
 		<td><a href="/src/dm/dm_generator.php">generate DM</a></td>
+		<td><a href="/src/dm/dm_delete.php">delete my DM</a></td>
     </tr>
 </table>
 <form method="POST" action="/src/dm/dm_finder.php">
