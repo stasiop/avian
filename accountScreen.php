ca07f02a015c104e18c1e11f534550ff3538d329
stasiop <51001942+stasiop@users.noreply.github.com>
2021-02-23 15:14:19 +0000

    Update accountScreen.php

--- accountScreen.php
+++ accountScreen.php
@@ -5,6 +5,19 @@ if(!isset($_COOKIE["user"])) {
 	setcookie($cookie_name, $cookie_value, time() + (86400 * 365), "/"); // 86400 = 1 day
 }
 ?>
+<style>
+body {
+  background-color: #545454;
+}
+table {
+	background-color: white;
+}
+p {
+	margin-right: 0%;
+	border: 10px;
+	color: white;
+}
+</style>
 <table border="1" class="table">
     <tr>
         <td><a href="/anonchat.php">anonchat</a></td>
@@ -16,3 +29,6 @@ if(!isset($_COOKIE["user"])) {
 	<input name="DMnum" placeholder="12345" type="text">
 	<button type="submit" value="submit">go to dm</button>
 </form>
+<p>
+This is the home of the Direct Messaging system designed by stan, it is currently very insecure.<br>Use at your own risk.
+</p>
