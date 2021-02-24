b0d99997a09a68999102d7935599f9a5459d79a6
stasiop <51001942+stasiop@users.noreply.github.com>
2021-02-24 20:39:46 +0000

    Update dm_finder.php

--- src/dm/dm_finder.php
+++ src/dm/dm_finder.php
@@ -19,6 +19,27 @@ if (!isset($_SESSION['DMnum'])) {
 </div>
 	<form method="POST" action="/src/dm/submitDM.php">
         <input name="message" placeholder="Isn't avian so pog?!" type="text">
+<?php
+session_start();
+if (isset($_POST['DMnum'])) {
+	$_SESSION['DMnum'] = $_POST['DMnum'];
+	$dmnum = $_SESSION['DMnum'];
+}
+if (!isset($_SESSION['DMnum'])) {
+	$_SESSION['DMnum'] = $_COOKIE['user'];
+	$dmnum = $_SESSION['DMnum'];
+}
+?>
+<title>DM | #<?php echo $_SESSION['DMnum'] ?></title>
+<link type="text/css" rel="stylesheet" href="/Stylesheet.css">
+<body onload="Autoscroll();">
+<div class="textContainer">
+	<iframe id="GFG" src = "/src/dm/<?php echo $_SESSION['DMnum'];?>.html" width = "60%" height = "87.5%"style="border: 0px">
+		Sorry your browser does not support inline frames. Use GNU IceCat.
+    </iframe>
+</div>
+	<form method="POST" action="/src/dm/submitDM.php">
+        <input name="message" placeholder="Isn't avian so pog?!" type="text">
         <button type="submit" value="submit">Send</button>
     </form>
 	<script>
@@ -46,36 +67,15 @@ if (!isset($_SESSION['DMnum'])) {
 		}
 	</script>
 </body>
+						<form action="/src/messageHandler/dm_upload.php" method="post" enctype="multipart/form-data">
+							  Select image to upload:
+							  <input type="file" name="fileToUpload" id="fileToUpload">
+							  <input type="submit" value="Upload Image" name="submit">
+						</form>
 <button id="myButton1" onclick="showJitsiCall()">show/hide call</button>
 <iframe id="vc" src="https://meet.jit.si/<?php echo $_SESSION['DMnum']; ?>" allow="camera;microphone" width="100%" height="500px">
 	your pc sucks
 </iframe>
-<!--
-hello
-hello
-hello
-hello
-hello
-hello
-hello
-hello
-hello
-hello
-hello
-hello
-hello
-hello
-hello
-hello
-hello
-hello
-hello
-hello
-hello
-hello
-hello
-hello
-hello
 hello
 hello
 hello
