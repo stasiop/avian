9da23079e77d39b434a4e5de6b38d532f5e5eb89
JohnBena <77627847+JohnBena@users.noreply.github.com>
2021-02-24 15:31:45 +0000

    Update anonchat.php

--- anonchat.php
+++ anonchat.php
@@ -24,7 +24,7 @@
                                                 xhttp.onreadystatechange = function() {
                                                 if (this.readyState == 4 && this.status == 200) {
                                                         serverID = this.responseText;
-                                                        console.log(serverID)
+                                                        //console.log(serverID)
                                                 }
                                                 };
                                                         xhttp.open("GET", "/src/messageHandler/id.txt", true);
@@ -33,62 +33,20 @@
                                         const task = async () => {
                                                 console.log("taskDone")
                                                 while(x) {
-                                                        
+                                                        //fuck i hate my fucking life
                                                         await new Promise(r => setTimeout(r, 1000));
                                                         console.log("serverID "+serverID)
                                                         console.log("clientID "+clientId)
+                                                        loadDoc();
                                                         if(clientId!=serverID){
                                                         
                                                                 console.log("differentID")
                                                                 clientId=serverID;
-                                                                loadDoc()
+                                                                //window.location.reload();
+                                                                
                                                                 
                                                         }else{
-                                                                console.log("sameID");
-                                                        };
-                                                        
-                                                };
-                                        }
-                                        task()
-                                        
-                                </script>                         
-                                      <iframe id="GFG" src = "/src/messageHandler/message.html" width = "100%" height = "100%"style="border: 0px">
-                                        Sorry your browser does not support inline frames. Use GNU IceCat.
-                                      </iframe>
-                        </div>
-                            <script>
-                                function Autoscroll() { // this is good
-                                var iframeID = document.getElementById("GFG");
-
-                                var iframeCW = (iframeID.contentWindow || iframeID.contentDocument);
-                                document.getElementById('GFG').onload = function(){ setTimeout("document.getElementById('GFG').contentWindow.scrollTo(0, 99999999)", 1) }
-                                if (iframeCW.document) iframeCW = iframeCW.document;
-                                iframeCW.body.style.color = "white";
-                                }
-                           </script>
-                           <script>
-                                Autoscroll();
-                            </script>
-                        <form method="POST" action="src/messageHandler/submit.php">
-
-                                <input name="message" placeholder="Isn't avian so pog?!" type="text">
-                                <button type="submit" value="submit">Send</button>
-                        </form>
-						<form action="/src/messageHandler/upload.php" method="post" enctype="multipart/form-data">
-							  Select image to upload:
-							  <input type="file" name="fileToUpload" id="fileToUpload">
-							  <input type="submit" value="Upload Image" name="submit">
-						</form>
-
-                      
-                </div>
-        </body>
-
-</html>
-
-
-
-                                                                console.log("sameID");
+                                                                //console.log("sameID");
                                                         };
                                                         
                                                 };
@@ -123,5 +81,7 @@
         </body>
 
 </html>
+
+
 
 
