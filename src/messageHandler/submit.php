7f67fbbcdfeaed340e990e3884436edc18014707
JohnBena <77627847+JohnBena@users.noreply.github.com>
2021-02-25 11:56:26 +0000

    Update submit.php

--- src/messageHandler/submit.php
+++ src/messageHandler/submit.php
@@ -2,12 +2,26 @@
 		$data_file = fopen("message.txt", "a+");
 		$message = $_POST["message"];
 		$idFile=fopen("id.txt","w+");
-		$id=(int)$idFile;
-		echo $id;
-		$id=$id+1;
-		echo $id;
-		fwrite($idFile,$id);
+		//$idContents=file_get_contents("ids.txt",true);
+		//echo $idContents;
+		$fileName="id.txt";
+		$idFileRead=fopen($fileName,"r");
+		$idContents=fread($idFileRead,"1024");
+		$idInt=(int)$idContents;
+		echo $idInt;
+		$idInt++;
+		echo $idInt;
+		fwrite($idFile,$idInt);
 		fclose($idFile);
+		fclose($idFileRead);
+		
+		//echo file_get_contents("id.txt");
+		//$id=(int)$idFile;
+		//echo $id;
+		//$id=$id+1;
+		//echo $id;
+		//fwrite($idFile,$id);
+		//fclose($idFile);
 		if (isset($_COOKIE['user'])) {
 			$name = "Anon (" . $_COOKIE['user'] . ")";
 		} else {
@@ -16,5 +30,5 @@
 		$text_to_write = $name . ": " . $message . "\n";
 		fwrite($data_file, $text_to_write);
 		fclose($data_file);
-		header('Location: /anonchat.php');
+		//header('Location: /anonchat.php');
 ?>
