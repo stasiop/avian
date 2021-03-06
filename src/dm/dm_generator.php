2daaa65eef17299d58b8e347e43b2fd93515af17
stasiop <51001942+stasiop@users.noreply.github.com>
2021-03-06 12:17:52 +0000

    Update dm_generator.php

--- src/dm/dm_generator.php
+++ src/dm/dm_generator.php
@@ -1,20 +1,21 @@
 <?php
 session_start();
 $_SESSION["DMnum"] = $_POST["DMnum"];
-if(!isset($_COOKIE["user"])) {
+if(!isset($_COOKIE["user"]) && !isset($_COOKIE["pamuser"])) {
         echo "YOU FOOL, YOUR COOKIE ISN'T SET";
 } else {
-        if (file_exists($_COOKIE["user"] . ".html")) {
+        if (isset($_COOKIE['pamuser'])) { $user=$_COOKIE['pamuser']; } elseif (isset($_COOKIE['user'])) { $user=$_COOKIE['user']; }
+        if (file_exists($user . ".html")) {
                 echo "The file exists";
         } else {
                 echo "doesn't exist (";
-                echo $_COOKIE["user"] . ".html)\n";
-                $myfile = fopen($_COOKIE["user"] . ".html", "a+") or die("\nUnable to create file!");
+                echo $user . ".html)\n";
+                $myfile = fopen($user . ".html", "a+") or die("\nUnable to create file!");
                 $txt = "";
                 fwrite($myfile, $txt);
                 fclose($myfile);
         }
 }
-$_SESSION["bdm"] = $_COOKIE["user"];
+$_SESSION["bdm"] = $user;
 header ('Location: dm_finder.php');
 ?>
