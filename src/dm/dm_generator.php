cc01c58a58445a3eeb0cb78ac5c7b5ef1805e540
stasiop <51001942+stasiop@users.noreply.github.com>
2021-03-06 12:27:31 +0000

    Update dm_generator.php

--- src/dm/dm_generator.php
+++ src/dm/dm_generator.php
@@ -4,7 +4,7 @@ $_SESSION["DMnum"] = $_POST["DMnum"];
 if(!isset($_COOKIE["user"]) && !isset($_COOKIE["pamuser"])) {
         echo "YOU FOOL, YOUR COOKIE ISN'T SET";
 } else {
-        if (isset($_COOKIE['pamuser'])) { $user=$_COOKIE['pamuser']; } elseif (isset($_COOKIE['user'])) { $user=$_COOKIE['user']; }
+        if (isset($_COOKIE['pamuser'])) { $user = substr(base64_decode($_COOKIE['pamuser']), 0, -6); } elseif (isset($_COOKIE['user'])) { $user=$_COOKIE['user']; }
         if (file_exists($user . ".html")) {
                 echo "The file exists";
         } else {
