1a670209adc1456ae9fe274f1b49b6e0f7db6f29
stasiop <51001942+stasiop@users.noreply.github.com>
2021-03-16 18:07:03 +0000

    Update dm_delete.php

--- src/dm/dm_delete.php
+++ src/dm/dm_delete.php
@@ -1,4 +1,12 @@
 <?php
-unlink($_COOKIE["user"] . ".html");
+if (isset($_COOKIE['pamuser'])){
+    $ufname = $_COOKIE['pamuser'];
+    $sfname = base64_decode($ufname);
+    $name = substr($sfname, 0, -6); #0000
+} else {
+    $name = "Anon";
+}
+
+unlink($name . ".html");
 header('Location: /accountScreen.php');
 ?>
