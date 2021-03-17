99f12b0863c9bcbe2802b2550ed4dcdc52a182a0
stasiop <51001942+stasiop@users.noreply.github.com>
2021-03-17 17:00:49 +0000

    Update dm_upload.php

--- src/messageHandler/dm_upload.php
+++ src/messageHandler/dm_upload.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 $target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$renamefileto = rand(0,2738912);
+$target_file = $target_dir . $renamefileto;
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 // Check if image file is a actual image or fake image
@@ -19,24 +20,26 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-	echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
-		if (isset($_COOKIE['pamuser'])){
-			$ufname = $_COOKIE['pamuser'];
-			$sfname = base64_decode($ufname);
-			$name = substr($sfname, 0, -6);	#0000
-		} elseif (isset($_COOKIE['user'])) { //what the fuck did you do??? are you aware that the cookie user is also set at accountScreen if not at PAM? it can try to decode a string of rndm numbers.
-			$name = $_COOKIE['user'];
-		} else {
-			$name = "Anon";
-		}
-		$data_file = fopen("../dm/" . $_SESSION['DMnum'] . ".html", "a+");
-		$text_to_write = "<br>" . $name . ':<br> <img src="/src/messageHandler/uploads/' . basename($_FILES["fileToUpload"]["name"]) . '">';
-		fwrite($data_file, $text_to_write);
-		fclose($data_file);
-		header('Location: /src/dm/dm_finder.php');
+        echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+                if (isset($_COOKIE['pamuser'])){
+                        $ufname = $_COOKIE['pamuser'];
+                        $sfname = base64_decode($ufname);
+                        $name = substr($sfname, 0, -6); #0000
+                } elseif (isset($_COOKIE['user'])) { //what the fuck did you do??? are you aware that the cookie user is also set at accountScreen if not at PAM? it can try to decode a string of rndm numbers.
+                        $name = $_COOKIE['user'];
+                } else {
+                        $name = "Anon";
+                }
+                $data_file = fopen("../dm/" . $_SESSION['DMnum'] . ".html", "a+");
+                $text_to_write = "<br>" . $name . ':<br> <img src="/src/messageHandler/uploads/' . $renamefileto . '">';
+                fwrite($data_file, $text_to_write);
+                fclose($data_file);
+
+                header('Location: /src/dm/dm_finder.php');
   } else {
     echo "Sorry, there was an error uploading your file.";
   }
 }
 }
 ?>
+
