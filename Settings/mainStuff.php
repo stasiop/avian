57296c3e65eb18bf14feef1e37c45e7781adcca5
JohnBena <77627847+JohnBena@users.noreply.github.com>
2021-03-24 17:15:23 +0000

    Create mainStuff.php

---
+++ Settings/mainStuff.php
@@ -0,0 +1,12 @@
+<h3>Themes</h3>
+<hr>
+<form method="POST">
+    <input type="file" id="fileInput" name="inputFile">
+    <input type="submit" id="fileSubmit" value="upload"> 
+</form>
+<?php 
+    if($_FILES['inputFile']){
+        $cssTheme= $_FILES['inputFile']
+        setcookie("customCSS",$cssTheme);
+    }    
+?>
