0ee23ce6d4b1aa28a977f2b6ee33d010f81bfaae
stasiop <51001942+stasiop@users.noreply.github.com>
2021-04-07 15:27:29 +0000

    Update submitDM.php

--- src/dm/submitDM.php
+++ src/dm/submitDM.php
@@ -13,26 +13,35 @@ $message = $_POST["message"];
                 if(strpos($message, "<") !== false){
                 echo "<h1>Dissallowed character</h1><br><hr><p>The character "<" is illegal due to exploits, for alternatives look at<br>our formatting help, thank you</p>";
                 } else {
-              if(empty(explode("(quote)", $message)) == false){
+              if(strpos($message, "(quote)") !== false){
                   $text_quoted=explode("(quote)", $message);
                   if(isset($text_quoted[1])){
                   $text_quoted[1]='<b class="quote">>' . $text_quoted[1];
                   } else {$text_quoted[1]="<b>" . $text_quoted[1];}
                   $message = $text_quoted[0] . $text_quoted[1];
-                  if(empty(explode("(/quote)", $message)) == false){
+                  if(strpos($message, "(/quote)") !== false){
                   $textquoted = explode("(/quote)", $message);
                   $message = $textquoted[0] . '</b>  ' . $textquoted[1];
                   } else {
                   $message = $message . '</b>';
                   }
+                  $total = "<br>" . $name . ": " . $message;
+                  $edited=1;
               }
-                if (empty(explode("(br)", $message)) == false){
+                if (strpos($message, "(br)") !== false){
                 if (substr_count($message,"(br)") > 1){header('Location: /anonchat.php');}else{
                 $textquoted = explode("(br)", $message);
-                $message = $textquoted[0] . '<br>    ' . $textquoted[1];
+                //echo "(br) found";
+                $message = $textquoted[0] . '<br>' . $textquoted[1];
+                $total = "<br>" . $name . ": " . $message;
+                $edited=1;
                 }
                 }
-                $text_to_write = $name . ": " . $message;
+                if ($edited==1){
+                        $text_to_write = $total;
+                } else {
+                        $text_to_write = "<br>" . $name . ": " . $message;
+                }
                 fwrite($data_file, $text_to_write);
                 echo "\n" . $message;
 }
