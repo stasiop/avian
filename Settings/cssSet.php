e4e0c10503b7f6193bbb5c595469b482bd2a8f84
stasiop <51001942+stasiop@users.noreply.github.com>
2021-04-08 14:02:11 +0000

    Create cssSet.php

---
+++ Settings/cssSet.php
@@ -0,0 +1,13 @@
+<?php
+$selected=&_POST['theme'];
+switch ($selected) {
+  case "W&B":
+    setcookie("style", "W&B", time() + (86400 * 365 * 4), "/"); // 86400 = 1 day
+    break;
+  case "P&P":
+    setcookie("style", "W&B", time() + (86400 * 365 * 4), "/"); // 86400 = 1 day
+    break;
+  case "def":
+    setcookie("style", "", time() + (86400 * 365 * 4), "/"); // 86400 = 1 day
+    break;
+}
