5ea289a0ea77aa941e94bc2dde5139637423d1af
stasiop <51001942+stasiop@users.noreply.github.com>
2021-04-08 13:43:06 +0000

    Update and rename CSSEditor.html to CSSEditor.php

---
+++ Settings/CSSEditor.php
@@ -0,0 +1,39 @@
+<title>Theme Selector</title>
+    <?php
+    if ($_COOKIE['style']=="P&P"){echo '<link rel="stylesheet" type="text/css" href="css/Stylesheet3PresetPinkAndPink.css">';}
+    if ($_COOKIE['style']=="W&B"){echo '<link rel="stylesheet" type="text/css" href="css/Stylesheet3PresetBlueAndWhite.css">';}
+    else { echo '<link rel="stylesheet" type="text/css" href="css/Stylesheet3.css">';}
+    ?>
+    <hr>
+    <form action="cssSet.php">
+      <label for="themes">Choose a theme:</label>
+      <select name="theme">
+        <option value="def">Defualt</option>
+        <option value="P&P">Pink</option>
+        <option value="W&B">White and Blue</option>
+      </select>
+      <br><br>
+      <input type="submit" value="Submit">
+    </form>
+<!--<head>
+    <title>CSS Editor</title>
+    <meta charset="utf-8"/>
+    <link rel="stylesheet" type="text/css" href="../css/Stylesheet4.css">
+</head>
+<body>
+    <script>
+        if(document.getElementById("cssInput").value!=null){
+            document.getElementById("cssInput").value=document.customCSS;
+        }
+        function applyChanges(){
+            textInput=document.getElementById("cssInput").value;
+            document.customCSS=textInput;
+        }
+
+    </script>
+    <form onsubmit="applyChanges()">
+        <textarea cols="10000" rows="20 "type="text" id="cssInput"></textarea>
+        <br>
+        <button type="submit">Apply</button>
+    </form>
+</body>
