3f782e6d2c8b1a0702512a2051c622407a6ca351
stasiop <51001942+stasiop@users.noreply.github.com>
2021-04-08 13:07:31 +0000

    Update Settings.php

--- Settings.php
+++ Settings.php
@@ -3,8 +3,8 @@
         Avian | Settings
     </Title>
     <?php
-    if ($_COOKIE['style']=="P&P"){echo '<link rel="stylesheet" type="text/css" href="css/Stylesheet3PresetPinkAndPink.css">;'}
-    if ($_COOKIE['style']=="W&B"){echo '<link rel="stylesheet" type="text/css" href="css/Stylesheet3PresetWhiteAndBlue.css">;'}
+    if ($_COOKIE['style']=="P&P"){echo '<link rel="stylesheet" type="text/css" href="css/Stylesheet3PresetPinkAndPink.css">';}
+    if ($_COOKIE['style']=="W&B"){echo '<link rel="stylesheet" type="text/css" href="css/Stylesheet3PresetWhiteAndBlue.css">';}
     else { echo '<link rel="stylesheet" type="text/css" href="css/Stylesheet3.css">;'}
     ?>
 </head>
