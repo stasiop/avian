6192b0cfddb8eb5d2863e91ab4b4cae6b1674758
stasiop <51001942+stasiop@users.noreply.github.com>
2021-04-08 13:08:12 +0000

    Update Settings.php

--- Settings.php
+++ Settings.php
@@ -5,7 +5,7 @@
     <?php
     if ($_COOKIE['style']=="P&P"){echo '<link rel="stylesheet" type="text/css" href="css/Stylesheet3PresetPinkAndPink.css">';}
     if ($_COOKIE['style']=="W&B"){echo '<link rel="stylesheet" type="text/css" href="css/Stylesheet3PresetWhiteAndBlue.css">';}
-    else { echo '<link rel="stylesheet" type="text/css" href="css/Stylesheet3.css">;'}
+    else { echo '<link rel="stylesheet" type="text/css" href="css/Stylesheet3.css">';}
     ?>
 </head>
 <body>
