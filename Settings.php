1678f652b208695937d1799c428abeffe8a9b485
stasiop <51001942+stasiop@users.noreply.github.com>
2021-04-08 13:06:16 +0000

    Update and rename Settings.html to Settings.php

---
+++ Settings.php
@@ -0,0 +1,21 @@
+<head>
+    <Title>
+        Avian | Settings
+    </Title>
+    <?php
+    if ($_COOKIE['style']=="P&P"){echo '<link rel="stylesheet" type="text/css" href="css/Stylesheet3PresetPinkAndPink.css">;'}
+    if ($_COOKIE['style']=="W&B"){echo '<link rel="stylesheet" type="text/css" href="css/Stylesheet3PresetWhiteAndBlue.css">;'}
+    else { echo '<link rel="stylesheet" type="text/css" href="css/Stylesheet3.css">;'}
+    ?>
+</head>
+<body>
+    <div id="settingSelector">
+        <button>General</button>
+        <button>Themes</button>
+    </div>
+	<div id="Vertical">
+	</div>
+    <iframe href="\Settings\mainStuff.php">
+        
+    </iframe>
+</body>
