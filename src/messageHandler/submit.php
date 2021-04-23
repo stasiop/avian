39e9a9eac870e2650da2f6b057fafa7b9e787b3e
stasiop <51001942+stasiop@users.noreply.github.com>
2021-04-23 18:52:33 +0000

    Update submit.php

--- src/messageHandler/submit.php
+++ src/messageHandler/submit.php
@@ -55,7 +55,7 @@
                         }
                         return $text_to_write;
                 }
-		function imageUpload($filename, $filetmpname, $postfile, $text_to_write1){
+                function imageUpload($filename, $filetmpname, $postfile, $text_to_write1){
                         $target_dir = "uploads/";
                         $renamefileto = rand(0,2738912);
                         if (file_exists("uploads/" . $renamefileto)){$renamefileto = rand(0,2738912);}
@@ -68,6 +68,10 @@
                                         echo "File is an image - " . $check["mime"] . ".";
                                         $uploadOk = 1;
                                         $uploadType = 1;
+                                } elseif (preg_match('/^.*\.(mp4|mov|mpg|mpeg|wmv|mkv)$/i', $filename)){
+                                        $uploadType = 2;
+                                        echo "File is an video";
+                                        $uploadOk = 1;
                                 } else {
                         echo "File is not an image.";
                                         $uploadOk = 0; // stan why is this not a bool
@@ -81,6 +85,9 @@
                                         echo "The file ". htmlspecialchars( basename($filename)). " has been uploaded.";
                                         if ($uploadType == 1){
                                                 $text_to_write1 = $text_to_write1 . '<br> <img src="/src/messageHandler/uploads/' . $renamefileto . '">';
+                                        } elseif ($uploadType == 2){
+                                                $text_to_write1 = $text_to_write1 . '<br> <video width="320" height="240" controls><source src="uploads/' . $renamefileto . '" type="' . mime_content_type('uploads/' . $renamefileto) . '">Your browser does not support the video tag.</video>';
+
                                         }
                                 } else {
                                         echo "There was an error uploading your file";
@@ -90,7 +97,7 @@
                         }
                 if (empty($_POST['message']) == false){echo "big chungus"; $snedited = 1; $text_to_write = formatCheck($name, $message);}
                 if (empty($_FILES['fileToUpload']['tmp_name']) == false){$snedited = 1; echo "big blungus"; $text_to_write = imageUpload($_FILES["fileToUpload"]["name"], $_FILES["fileToUpload"]["tmp_name"], $_POST["fileToUpload"], $text_to_write);}
-                if ($snedited == 0){header('Location: /anonchat.php');}
+                if ($snedited == 0){$text_to_write = ''; header('Location: /anonchat.php');}
                 fwrite($data_file, $text_to_write);
                 echo "<br>Your message was: " . $message . "<br>";
                 echo "Your text written was: " . $text_to_write . "<br>";
