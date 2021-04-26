051bd12d71d4a8532c8103eac33edf4ea8551f79
stasiop <51001942+stasiop@users.noreply.github.com>
2021-04-26 16:16:08 +0000

    Update delete_old_files.py

--- delete_old_files.py
+++ delete_old_files.py
@@ -1,12 +1,12 @@
 import os.path
 import os
 import time
-path="/var/www/html/src/messageHandler/uploads"
+path="/var/www/html/src/messageHandler/uploads/"
 filelist = []
 print ("current time:", time.time())
 for (root, dirs, file) in os.walk(path):
         for f in file:
-                print("looking at file: ",f, "  date modified: ", os.path.getmtime(f))
-                if os.path.getmtime(f) < time.time()-2592000:
-                        os.remove(f)
+                print("looking at file: ",f, "  date modified: ", os.path.getmtime(path+f))
+                if os.path.getmtime(path+f) < time.time()-2592000:
+                        os.remove(path+f)
                         print("deleted: ",f)
