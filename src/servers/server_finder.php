fbbfcbb3ed2d4ca39ed24e3f0be4ebe148c466f4
stasiop <51001942+stasiop@users.noreply.github.com>
2021-05-06 17:17:25 +0000

    Update server_finder.php

--- src/servers/server_finder.php
+++ src/servers/server_finder.php
@@ -1,53 +1,38 @@
-<style>                                                                                                                                                                                                                                                  
-table, th, td {                                                                                                                                                                                                                                          
-  border: 1px solid black;                                                                                                                                                                                                                               
-}                                                                                                                                                                                                                                                        
-</style>                                                                                                                                                                                                                                                 
-<table style="width='100%'"><!--setup user list-->                                                                                                                                                                                                       
-<?php                                                                                                                                                                                                                                                    
-session_start();                                                                                                                                                                                                                                         
-$my_sql_pass=""; // enter pass                                                                                                                                                                                                                           
-$con = mysqli_connect("127.0.0.1","root",$my_sql_pass,"server_nfo");                                                                                                                                                                                     
-                                                                                                                                                                                                                                                         
-if (mysqli_connect_errno()) {                                                                                                                                                                                                                            
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();                                                                                                                                                                                          
-  exit();                                                                                                                                                                                                                                                
-}                                                                                                                                                                                                                                                        
-                                                                                                                                                                                                                                                         
-// Perform query                                                                                                                                                                                                                                         
-                                                                                                                                                                                                                                                         
-if ($result = mysqli_query($con, 'SELECT server_users FROM server_nfo WHERE server_id="' . $_SESSION['server_selected'] . '"')) {                                                                                                                        
-  while ($row = mysqli_fetch_row($result)){                                                                                                                                                                                                              
-        if(strpos($row[0], ",")){                                                                                                                                                                                                                        
-                $result = explode("],[", $row[0]);                                                                                                                                                                                                       
-                echo $result[0] . $result[1];                                                                                                                                                                                                            
-        /*      foreach ($result as $value) {                                                                                                                                                                                                            
-                        no peeking :)                                                                                                                                                                                                                    
-                        if ($result = mysqli_query($con, 'SELECT Username FROM users WHERE UniversalID=' . $value )) {                                                                                                                                   
-                                 while ($row = mysqli_fetch_row($result)){                                                                                                                                                                               
-                                        $username = $row[0];                                                                                                                                                                                             
-                                 }                                                                                                                                                                                                                       
-                                 mysqli_close($con);                                                                                                                                                                                                     
-                        } else {                                                                                                                                                                                                                         
-                        echo "failure";                                                                                                                                                                                                                  
-                        $username = $row[0];}                                                                                                                                                                                                            
-                        echo "<tr><th>" . $username . "</th></tr>";                                                                                                                                                                                      
-                }                                                                                                                                                                                                                                        
-        } else {                                                                                                                                                                                                                                         
-                $con = mysqli_connect("127.0.0.1","root","deez nuts","login_system");                                                                                                                                                                    
-                if ($result = mysqli_query($con, 'SELECT Username FROM users WHERE UniversalID=' . $row[0])) {                                                                                                                                           
-                        while ($row = mysqli_fetch_row($result)){                                                                                                                                                                                        
-                                $username = $row[0];                                                                                                                                                                                                     
-                        }                                                                                                                                                                                                                                
-                                echo "<tr><th>" . $username . "</th></tr>";                                                                                                                                                                              
-                                mysqli_close($con);                                                                                                                                                                                                      
-                        }                                                                                                                                                                                                                                
-        }*/                                                                                                                                                                                                                                              
-  }                                                                                                                                                                                                                                                      
-  // Free result set                                                                                                                                                                                                                                     
-}                                                                                                                                                                                                                                                        
-$con = mysqli_connect("127.0.0.1","root",$my_sql_pass,"server_nfo");                                                                                                                                                                                     
-mysqli_close($con);                                                                                                                                                                                                                                      
-}                                                                                                                                                                                                                                                        
-?>                                                                                                                                                                                                                                                       
-</table> 
+table, th, td {
+  border: 1px solid black;
+}
+</style>
+<table style="width='100%'"><!--setup user list-->
+<?php
+session_start();
+$my_sql_pass=""; // enter pass
+$con = mysqli_connect("127.0.0.1","root",$my_sql_pass,"server_nfo");
+
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
+}
+if ($result = mysqli_query($con, 'SELECT server_users FROM server_nfo WHERE server_invite="' . $_SESSION['server_selected'] . '"')) {
+        if ($row = mysqli_fetch_row(mysqli_query($con, 'SELECT server_users FROM server_nfo WHERE server_invite="' . $_SESSION['server_selected'] . '"'))){
+                if(strpos($row[0], ",")){
+
+                $result = explode(",", $row[0]);
+                foreach ($result as $value){
+                        $value = str_replace( array( '[', ']'), "", $value);
+                        $con = mysqli_connect("127.0.0.1","root",$my_sql_pass,"login_system");
+                        if ($result = mysqli_query($con, 'SELECT Username FROM users WHERE UniversalID=' . $value)) {
+                        if ($row = mysqli_fetch_row($result)){
+                        $value = $row[0];
+                        }
+                        }
+                        $username = $value;
+                        echo "<tr><th>" . $username . "</th></tr>";
+                }
+        }
+  }else{echo "pee";}
+  // Free result set
+} else {echo "IS THIS PEE AND POO";}
+//} else{echo "bad mysql";}
+
+?>
+</table>
