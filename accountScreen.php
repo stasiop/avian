f942472d99c8a198d5d6a52203ce2774a06a3d01
stasiop <51001942+stasiop@users.noreply.github.com>
2021-05-08 16:31:13 +0000

    Update accountScreen.php

--- accountScreen.php
+++ accountScreen.php
@@ -37,15 +37,8 @@ if(!isset($_COOKIE["user"])) {
 <p id="info">
 This is the home of the Direct Messaging system designed by stan, it is currently very insecure.<br>Use at your own risk.
 </p>
-~                                                                                                                                                                                                                                             
-~                                                                                                                                                                                                                                             
-~                                                                                                                                                                                                                                             
-~                                                                                                                                                                                                                                             
-~                                                                                                                                                                                                              
-~                                                                                                                                                                                                                                             
-~                                                                                                                                                                                                                                             
-~                                                                                                                                                                                                                                             
-~                                  
+<?php>
+        if(false){
                 echo "<link type='text/css' rel='stylesheet' href=''.$PATH.''";
 
         }else{
@@ -53,24 +46,3 @@ This is the home of the Direct Messaging system designed by stan, it is currentl
         }
         
 ?>                  
-        <a href="/anonchat.php" id="anon">Global Chat</a>
-
-<div id="spaceHog"></div>
-<form method="POST" action="/src/dm/dm_finder.php">
-<div id="inputBorder">  
-
-        
-        <input name="DMnum" placeholder="12345" type="text" id="dmNameInput">
-        <button type="submit" value="submit" id="dmNameSubmit">Go To DM</button>
-        </div>
-
-</form>
-<div id="spaceHog"></div>
-<a href="/src/dm/dm_generator.php" id="generate">Create a DM</a>
-                <a href="/src/dm/dm_delete.php" id="delete">Remove a DM</a>
-<p id="info">
-This is the home of the Direct Messaging system designed by stan, it is currently very insecure.<br>Use at your own risk.
-</p>
-~                                                                                                                                                                                                                                             
-~                                                                                                                                                                                                                                             
-~            
